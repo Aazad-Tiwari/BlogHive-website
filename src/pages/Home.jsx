72b8904d7dc9d8084fca472867f5954a5ce3e4c3
Aazad-Tiwari <aazadtiwari15@gmail.com>
2024-12-03 21:34:03 +0530

    fix recent post logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,6 @@ import { Link } from 'react-router-dom'
 function Home() {
 
     const userData = useSelector(state => state.auth.userData)
-    const lastPostFromStore = useSelector(state => state.posts.lastPost)
-    const [lastPost, setLastPost] = useState(lastPostFromStore);
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
@@ -24,21 +22,11 @@ function Home() {
             })
     }, [])
 
-    useEffect(() => {
-        if (lastPostFromStore) {
-            setLastPost(lastPostFromStore);
-            appwriteService.getPost(lastPostFromStore)
-                .then((response) => {
-                    if (response) {
-                        setLastPost(response);
-                    }
-                });
-        }
-    }, [lastPostFromStore]);
+    
 
 
 
-   
+
     function truncateHTML(htmlString, maxlength) {
         const textContent = new DOMParser().parseFromString(htmlString, 'text/html').body.textContent || '';
 
@@ -49,16 +37,16 @@ function Home() {
 
     function formatDateString(dateString) {
         if (!dateString) return null; // Handle cases where the input is undefined or null
-    
+
         const date = new Date(dateString); // Convert the date string to a Date object
-    
+
         return new Intl.DateTimeFormat('en-GB', {
             day: '2-digit',
             month: 'long',
             year: 'numeric',
         }).format(date); // Format the date
     }
-    
+
 
     // const truncatedText = truncateHTML(String(lastPost.content), 250)
     // console.log(truncatedText);
@@ -82,35 +70,41 @@ function Home() {
             </div>
 
 
-            {lastPost && userData ?
+            {userData ?
                 <>
                     <h1 className='text-4xl font-bold text-blog_black absolute mt-20 ml-10 underline'>Our Recent Posts</h1>
-                    <div className='relative w-full max-w-[1232px] h-[636px] mt-36 mx-auto rounded-xl'>
-                        <img src={lastPost?.featuredImage ? appwriteService.getFilePreview(lastPost.featuredImage) : null} alt="" className='w-full h-[530px] object-cover object-center rounded-xl' />
-                        <div className='absolute w-[920px] h-[320px] bottom-0 right-0 rounded-lg bg-white shadow flex flex-col justify-around pl-10'>
-                            <p className=' leading-[150%] text-sm font-medium text-[#999999] w-32 h-1  '>{formatDateString(lastPost.$createdAt)}</p>
-                            <h2 className=' w-[750px] h-2 text-3xl leading-[10px] text-left tracking-[-1px] font-bold text-blog_black'>{lastPost.title}</h2>
-                            <p className='text-base text-[#666666] tracking-normal text-left leading-[150%] w-[750px] h-[70px]'>{ truncateHTML(String(lastPost.content), 250) }</p>
-                            <button className='w-fit px-6 py-2 border-[2px] rounded-md text-blog_blue font-semibold border-blog_blue'> <Link to={`/post/${lastPost.$id}`} >Read More</Link> </button>
-                        </div>
-                    </div>
+                    {
+                        posts.map((post, index) => (
+                            index < 1 ?
+                                <div className='relative w-full max-w-[1232px] h-[636px] mt-36 mx-auto rounded-xl'>
+                                    <img src={post?.featuredImage ? appwriteService.getFilePreview(post.featuredImage) : null} alt="" className='w-full h-[530px] object-cover object-center rounded-xl' />
+                                    <div className='absolute w-[920px] h-[320px] bottom-0 right-0 rounded-lg bg-white shadow flex flex-col justify-around pl-10'>
+                                        <p className=' leading-[150%] text-sm font-medium text-[#999999] w-32 h-1  '>{formatDateString(post.$createdAt)}</p>
+                                        <h2 className=' w-[750px] h-2 text-3xl leading-[10px] text-left tracking-[-1px] font-bold text-blog_black'>{post.title}</h2>
+                                        <p className='text-base text-[#666666] tracking-normal text-left leading-[150%] w-[750px] h-[70px]'>{truncateHTML(String(post.content), 250)}</p>
+                                        <button className='w-fit px-6 py-2 border-[2px] rounded-md text-blog_blue font-semibold border-blog_blue'> <Link to={`/post/${post.$id}`} >Read More</Link> </button>
+                                    </div>
+                                </div> : null
+                        ))
+                    }
+
                 </> : null}
 
 
-            {userData && lastPost ?  
+            {userData ?
                 <div className='relative max-w-[1234px] mx-auto grid grid-cols-3 gap-5 items-center justify-center gap-y-14 mt-14'>
                     {posts.map((post, index) => (
                         index < 6 ?
-                        <div key={post.$id} className=' mx-auto rounded-lg'>
-                            <PostCard {...post} time={formatDateString(post.$createdAt)} truncatedContent={truncateHTML(post.content,250)} />
-                        </div>
-                        : null
+                            <div key={post.$id} className=' mx-auto rounded-lg'>
+                                <PostCard {...post} time={formatDateString(post.$createdAt)} truncatedContent={truncateHTML(post.content, 250)} />
+                            </div>
+                            : null
                     ))}
                 </div>
-             : null}
+                : null}
 
 
-            { userData ? <Subscribe /> : <Subscribe content='Please Login to read our recent blogs' /> }
+            {userData ? <Subscribe /> : <Subscribe content='Please Login to read our recent blogs' />}
 
         </div>
     )
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -15,11 +15,13 @@ const authSlice = createSlice({
         login : (state, action) => {
             state.status = true
             state.userData = action.payload
+            localStorage.setItem('userData', JSON.stringify(action.payload) )
         },
 
         logout : (state) => {
             state.status = false
             state.userData = null
+            localStorage.removeItem('userData')
         },
     }
 })
