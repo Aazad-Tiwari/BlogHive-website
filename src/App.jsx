eca0eae934d4a229d7be140c14a278845739af67
Aazad-Tiwari <aazadtiwari15@gmail.com>
2024-12-19 11:42:07 +0530

    fix session issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { login, logout, initializeUserData } from './store/authSlice';
-import React, { useEffect, useState } from 'react'
+import React , { useEffect, useState } from 'react'
 import authService from './appwrite/auth_service'
 import { useDispatch } from 'react-redux';
-import { Header, Footer, Loading } from './components/'
-import { Outlet } from 'react-router-dom'
+import {Header, Footer, Loading} from './components/'
+import {Outlet} from 'react-router-dom'
 import './App.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
@@ -13,49 +13,48 @@ function App() {
   // console.log(import.meta.env.VITE_APPWRITE_URL);  // FOR VITE
 
   // console.log(import.meta.env.VITE_API_KEY);
-
+  
 
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    const fetchAndValidateUser = async () => {
-      try {
-        dispatch(initializeUserData());
-        if (localStorage.getItem('userData')) {
-          const userData = await authService.getCurrentUser();
-          if (userData) {
-            dispatch(login(userData));
-          } else {
-            dispatch(logout());
-          }
-        }
-      } catch (error) {
-        console.error("Error validating user session:", error);
-        dispatch(logout());
-      } finally {
-        setLoading(false);
-      }
-    };
+   useEffect(() => {
+        const fetchAndValidateUser = async () => {
+            try {
+                dispatch(initializeUserData());
 
-    fetchAndValidateUser();
-  }, [dispatch]);
+                const userData = await authService.getCurrentUser();
+                if (userData) {
+                    dispatch(login(userData)); 
+                } else {
+                    dispatch(logout());
+                }
+            } catch (error) {
+                console.error("Error validating user session:", error);
+                dispatch(logout());
+            } finally {
+                setLoading(false); 
+            }
+        };
 
+        fetchAndValidateUser();
+    }, [dispatch]);
 
+ 
   return !loading ? <div className=' flex  content-center overflow-x-hidden' >
     <div className='w-full block'>
-      <Header />
-      <Outlet />
-      <Footer />
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        closeOnClick
-        pauseOnHover
-      />
+      <Header/>
+      <Outlet/>
+      <Footer/>
+      <ToastContainer 
+          position="top-center" 
+          autoClose={2000} 
+          hideProgressBar={false} 
+          closeOnClick 
+          pauseOnHover 
+        />
     </div>
-  </div> : <Loading />
+  </div> : <Loading/>
 }
 
 export default App
