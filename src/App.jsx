d53bf4045e7abed7365c126b96398ab631704fe7
Aazad-Tiwari <aazadtiwari15@gmail.com>
2024-12-19 11:17:54 +0530

    fix main bug appwrite session exist

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { login, logout, initializeUserData } from './store/authSlice';
-import React , { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import authService from './appwrite/auth_service'
-import { useDispatch, useSelector } from 'react-redux';
-import {Header, Footer, Loading} from './components/'
-import {Outlet} from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { Header, Footer, Loading } from './components/'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
@@ -13,55 +13,49 @@ function App() {
   // console.log(import.meta.env.VITE_APPWRITE_URL);  // FOR VITE
 
   // console.log(import.meta.env.VITE_API_KEY);
-  
+
 
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch()
-  const authStatus = useSelector(state => state.auth.status)
 
   useEffect(() => {
-    dispatch(initializeUserData() )
-
-    if (authStatus) {
-      const fetchCurrentUser = async () => {
-        try {
-          const userData = await authService.getCurrentUser(); 
+    const fetchAndValidateUser = async () => {
+      try {
+        dispatch(initializeUserData());
+        if (localStorage.getItem('userData')) {
+          const userData = await authService.getCurrentUser();
           if (userData) {
             dispatch(login(userData));
-            
           } else {
             dispatch(logout());
           }
-        } catch (err) {
-          dispatch(logout());
-        } finally {
-          setLoading(false);
         }
-      };
+      } catch (error) {
+        console.error("Error validating user session:", error);
+        dispatch(logout());
+      } finally {
+        setLoading(false);
+      }
+    };
 
-      fetchCurrentUser();
-    } else {
-      // If not logged in, directly set loading to false
-      setLoading(false);
-    }
-  }, [dispatch, authStatus]); // Only run this effect when authStatus changes
+    fetchAndValidateUser();
+  }, [dispatch]);
 
 
- 
   return !loading ? <div className=' flex  content-center overflow-x-hidden' >
     <div className='w-full block'>
-      <Header/>
-      <Outlet/>
-      <Footer/>
-      <ToastContainer 
-          position="top-center" 
-          autoClose={2000} 
-          hideProgressBar={false} 
-          closeOnClick 
-          pauseOnHover 
-        />
+      <Header />
+      <Outlet />
+      <Footer />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+      />
     </div>
-  </div> : <Loading/>
+  </div> : <Loading />
 }
 
 export default App
